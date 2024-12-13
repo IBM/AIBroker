9c952549c742fd5c5be7cdd5bdaa3218174f1521
MANISH123830 <sai.manjunath@ibm.com>
2024-12-13 13:00:02 +0530

    Delete dependencies 3 directory

--- dependencies 3/minio-deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-oc apply -f minio/minio-ns.yml
-oc apply -f minio/minio-pvc.yml
-oc apply -f minio/minio-deployment.yml
-oc apply -f minio/minio-service.yml
-oc apply -f minio/minio-route.yml
